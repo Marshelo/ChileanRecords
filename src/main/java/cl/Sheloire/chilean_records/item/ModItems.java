9f92adf685821a3fd392e3592f231503857616a6
Shelo <correodeshelo@gmail.com>
2022-09-13 01:58:06 -0300

    lol

--- src/main/java/cl/Sheloire/chilean_records/item/ModItems.java
+++ src/main/java/cl/Sheloire/chilean_records/item/ModItems.java
@@ -15,9 +15,6 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, ChileanRecords.MOD_ID);
 
-//    public static final RegistryObject<Item> ITEM_NAME = ITEMS.register("item_name",
-//            () -> new Item[or what you what](new Item[again].Properties().tab(CreativeModeTab.TAB_MISC)));
-
     public static final RegistryObject<Item> MUSIC_DISC_CHILEAN_ANTHEM = ITEMS.register("music_disc_chilean_anthem",
             () -> new RecordItem(1, ModSounds.CHILEAN_ANTHEM,
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1)));
@@ -28,6 +25,30 @@ public class ModItems {
 
     public static final RegistryObject<Item> MUSIC_DISC_LOS_VIEJOS_ESTANDARTES = ITEMS.register("music_disc_los_viejos_estandartes",
             () -> new RecordItem(3, ModSounds.LOS_VIEJOS_ESTANDARTES,
+                    new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1)));
+
+    public static final RegistryObject<Item> MUSIC_DISC_UN_ANO_MAS = ITEMS.register("music_disc_un_ano_mas",
+            () -> new RecordItem(4, ModSounds.UN_ANO_MAS,
+                    new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1)));
+
+    public static final RegistryObject<Item> MUSIC_DISC_TREN_AL_SUR = ITEMS.register("music_disc_tren_al_sur",
+            () -> new RecordItem(5, ModSounds.TREN_AL_SUR,
+                    new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1)));
+
+    public static final RegistryObject<Item> MUSIC_DISC_FALSO_AMOR = ITEMS.register("music_disc_falso_amor",
+            () -> new RecordItem(6, ModSounds.FALSO_AMOR,
+                    new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1)));
+
+    public static final RegistryObject<Item> MUSIC_DISC_CURAO_MANEJO_MEJOR = ITEMS.register("music_disc_curao_manejo_mejor",
+            () -> new RecordItem(7, ModSounds.CURAO_MANEJO_MEJOR,
+                    new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1)));
+
+    public static final RegistryObject<Item> MUSIC_DISC_LA_MEJICANA = ITEMS.register("music_disc_la_mejicana",
+            () -> new RecordItem(8, ModSounds.LA_MEJICANA,
+                    new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1)));
+
+    public static final RegistryObject<Item> MUSIC_DISC_MORTAL_KUMBIAT = ITEMS.register("music_disc_mortal_kumbiat",
+            () -> new RecordItem(9, ModSounds.MORTAL_KUMBIAT,
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1)));
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
