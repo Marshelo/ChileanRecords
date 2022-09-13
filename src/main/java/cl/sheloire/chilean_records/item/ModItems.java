cf33cfa6e7fab236d2c9912475c4f5ee79a13eb3
Shelo <correodeshelo@gmail.com>
2022-09-13 17:30:56 -0300

    Fixed the output signal of the comparator for each disc and updated description of the mod

--- src/main/java/cl/sheloire/chilean_records/item/ModItems.java
+++ src/main/java/cl/sheloire/chilean_records/item/ModItems.java
@@ -23,42 +23,42 @@ public class ModItems {
 
     public static final RegistryObject<Item> MUSIC_DISC_LA_CONSENTIDA =
             ITEMS.register("music_disc_la_consentida",
-                    ()-> new MusicDiscItem(1, () -> ModSounds.LA_CONSENTIDA.get(),
+                    ()-> new MusicDiscItem(2, () -> ModSounds.LA_CONSENTIDA.get(),
                             new Item.Properties().maxStackSize(1).group(ItemGroup.MISC)));
 
     public static final RegistryObject<Item> MUSIC_DISC_LOS_VIEJOS_ESTANDARTES =
             ITEMS.register("music_disc_los_viejos_estandartes",
-                    ()-> new MusicDiscItem(1, () -> ModSounds.LOS_VIEJOS_ESTANDARTES.get(),
+                    ()-> new MusicDiscItem(3, () -> ModSounds.LOS_VIEJOS_ESTANDARTES.get(),
                             new Item.Properties().maxStackSize(1).group(ItemGroup.MISC)));
 
     public static final RegistryObject<Item> MUSIC_DISC_UN_ANO_MAS =
             ITEMS.register("music_disc_un_ano_mas",
-                    ()-> new MusicDiscItem(1, () -> ModSounds.UN_ANO_MAS.get(),
+                    ()-> new MusicDiscItem(4, () -> ModSounds.UN_ANO_MAS.get(),
                             new Item.Properties().maxStackSize(1).group(ItemGroup.MISC)));
 
     public static final RegistryObject<Item> MUSIC_DISC_TREN_AL_SUR =
             ITEMS.register("music_disc_tren_al_sur",
-                    ()-> new MusicDiscItem(1, () -> ModSounds.TREN_AL_SUR.get(),
+                    ()-> new MusicDiscItem(5, () -> ModSounds.TREN_AL_SUR.get(),
                             new Item.Properties().maxStackSize(1).group(ItemGroup.MISC)));
 
     public static final RegistryObject<Item> MUSIC_DISC_FALSO_AMOR =
             ITEMS.register("music_disc_falso_amor",
-                    ()-> new MusicDiscItem(1, () -> ModSounds.FALSO_AMOR.get(),
+                    ()-> new MusicDiscItem(6, () -> ModSounds.FALSO_AMOR.get(),
                             new Item.Properties().maxStackSize(1).group(ItemGroup.MISC)));
 
     public static final RegistryObject<Item> MUSIC_DISC_CURAO_MANEJO_MEJOR =
             ITEMS.register("music_disc_curao_manejo_mejor",
-                    ()-> new MusicDiscItem(1, () -> ModSounds.CURAO_MANEJO_MEJOR.get(),
+                    ()-> new MusicDiscItem(7, () -> ModSounds.CURAO_MANEJO_MEJOR.get(),
                             new Item.Properties().maxStackSize(1).group(ItemGroup.MISC)));
 
     public static final RegistryObject<Item> MUSIC_DISC_LA_MEJICANA =
             ITEMS.register("music_disc_la_mejicana",
-                    ()-> new MusicDiscItem(1, () -> ModSounds.LA_MEJICANA.get(),
+                    ()-> new MusicDiscItem(8, () -> ModSounds.LA_MEJICANA.get(),
                             new Item.Properties().maxStackSize(1).group(ItemGroup.MISC)));
 
     public static final RegistryObject<Item> MUSIC_DISC_MORTAL_KUMBIAT=
             ITEMS.register("music_disc_mortal_kumbiat",
-                    ()-> new MusicDiscItem(1, () -> ModSounds.MORTAL_KUMBIAT.get(),
+                    ()-> new MusicDiscItem(9, () -> ModSounds.MORTAL_KUMBIAT.get(),
                             new Item.Properties().maxStackSize(1).group(ItemGroup.MISC)));
 
     public static void register(IEventBus eventBus) {
